a5e59db223c8bd4cfb0f9ac0c9134401ce19f697
DamVietHieu <104876647+DamVietHieu@users.noreply.github.com>
2022-05-29 11:28:52 -0500

    Add files via upload

---
+++ TestCase/testcase.js
@@ -0,0 +1,12 @@
+const menubar = document.querySelector(".header-top>i")
+const closeBtn = document.querySelector(".header-top ul i")
+// menubar.addEventListener("click",function (){
+//     document.querySelector(".header-top ul").style.tranform="translateX(0%)"
+//     document.querySelector(".header-top ul").style.opacity="1"
+// })
+// closeBtn.addEventListener("click",function () {
+//     document.querySelector(".header-top ul").style.tranform="translateX(10%)"
+//     document.querySelector(".header-top ul").style.opacity="0"
+// })
+
+
